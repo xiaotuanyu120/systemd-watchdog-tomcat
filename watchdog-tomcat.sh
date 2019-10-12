e8ecd61d5188c8ee0bd3155ea54a0d647cdf40f8
TMP <zhaopeiwu@outlook.com>
2019-10-13 00:06:45 +0800

    add README and update logic

--- watchdog-tomcat.sh
+++ watchdog-tomcat.sh
@@ -10,44 +10,59 @@ CATALINA_BASE=/usr/local/tomcat
 trap "rm -f ${CURL_TMP_RESULT}" EXIT
 
 getPidByPort() {
-    port=$1
     pid_raw=`ss -lnpt|grep ":$WATCHED_PORT "|awk '{print $6}'|awk -F "pid=" '{print $2}'|awk -F "," '{print $1}'`
 
-    if [[ -n $pid_raw ]] ; then
+    [[ -n $pid_raw ]] && {
         space_regex=".* .*"
         if [[ $pid_raw =~ $space_regex ]] ; then
             pid=`echo $pid_raw|awk '{print $1}'`
         else
             pid=$pid_raw
         fi
-    else
-       pid="null"
-    fi
+    }
+}
+
+healthCheck() {
+    # limit whole check time in 8 seconds and connect time in 2 seconds
+    curl -s --connect-timeout 2 --max-time 8 -o $CURL_TMP_RESULT $WATCHED_NET && HEALTH_STATUS="success" || HEALTH_STATUS="fail"
 }
 
 watchdogTomcat() {
+    # INITIAL OF WATCHDOG
+    # go to watchdog logic when conditions down satisfied
+    #   - pid exist
+    #   - first health check status success
     while : ; do
-        getPidByPort $WATCHED_PORT
-        [[ $pid != "null" ]] && systemd-notify --ready ; sleep 2 ; break
+        [[ -n $pid ]] && {
+            healthCheck
+            [[ $HEALTH_STATUS -eq "success" ]] && {
+                echo "healthCheck >>> notify systemd READY=1" # debug
+                systemd-notify --pid=$pid --ready
+                break
+            }
+        } || {
+            getPidByPort
+            #echo "getPidByPort >>> PID_RAW=$pid_raw PID:$pid"             # debug
+        }
     done
 
+    # WATCHDOG START
     while : ; do
-        FAIL=0
         interval=$(($WATCHDOG_USEC / $((2 * 1000000))))
-        curl -s -o $CURL_TMP_RESULT $WATCHED_NET || FAIL=1
 
-        if [[ $FAIL -eq 0 ]] ; then
-            echo "watchdog detect success" # debug
-            systemd-notify WATCHDOG=1
+        healthCheck
+
+        if [[ $HEALTH_STATUS -eq "success" ]] ; then
+            #echo "watchdog detect success" # debug
+            systemd-notify --pid=$pid WATCHDOG=1
             sleep ${interval}
         else
-            echo "watchdog detect failed" # debug
+            #echo "watchdog detect failed" # debug
             sleep 1
         fi
     done
 }
 
 
-
+${CATALINA_BASE}/bin/catalina.sh start
 watchdogTomcat &
-eval exec ${CATALINA_BASE}/bin/catalina.sh start
